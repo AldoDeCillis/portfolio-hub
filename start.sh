0710f4ebb6df4e18cccd82fad47e8e6e4b0f14a5
aldodecillis <aldo.decillis00@gmail.com>
2025-06-02 17:12:05 +0200

    test

--- start.sh
+++ start.sh
@@ -1,7 +1,32 @@
-#!/bin/sh
+# start.sh
+#!/bin/bash
 
-# Start PHP-FPM in the background
-php-fpm &
+# Crea dinamicamente il file .env
+cat <<EOF > .env
+APP_NAME=Laravel
+APP_ENV=${APP_ENV}
+APP_KEY=${APP_KEY}
+APP_DEBUG=${APP_DEBUG}
+APP_URL=${APP_URL}
 
-# Start nginx in foreground
+LOG_CHANNEL=stack
+
+DB_CONNECTION=mysql
+DB_HOST=${DB_HOST}
+DB_PORT=${DB_PORT}
+DB_DATABASE=${DB_DATABASE}
+DB_USERNAME=${DB_USERNAME}
+DB_PASSWORD=${DB_PASSWORD}
+
+SESSION_DRIVER=file
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
+EOF
+
+# Imposta permessi corretti
+chmod -R 775 storage bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+
+# Avvia PHP-FPM e Nginx
+service php8.2-fpm start
 nginx -g 'daemon off;'
